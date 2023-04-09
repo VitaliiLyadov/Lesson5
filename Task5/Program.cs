37df6b772dcbad33108b5a21e0c368b5d5782f56
Vitalii <LVitek2506@yandex.ru>
2023-04-09 09:40:55 +0500

    add Task1-5

---
+++ Task5/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 4: Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+int ReadInt(string msg)
+{
+    System.Console.Write($"{msg} > ");
+    return int.Parse(Console.ReadLine());
+}
+
+int[] CreateArray(int length, int minRnd, int maxRnd)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(minRnd, maxRnd + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+int[] multiP(int[] array)
+{
+    int[] res = new int[array.Length / 2 + array.Length % 2];
+    res[res.Length-1] = array[res.Length-1];
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        res[i] = array[i] * array[array.Length - 1 - i];
+    }
+    return res;
+}
+
+
+int len = ReadInt("Введите длину массива");
+int minRnd = ReadInt("Введите границу минимума случайной ведичины");
+int maxRnd = ReadInt("Введите границу максимума случайной ведичины");
+int[] array = CreateArray(len, minRnd, maxRnd);
+PrintArray(array);
+PrintArray(multiP(array));
